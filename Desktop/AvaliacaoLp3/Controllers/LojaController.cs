2319eec0f8b3cfa37bf24e54e0957119942216d2
sabrina-reis1 <101716378+sabrina-reis1@users.noreply.github.com>
2022-09-19 01:15:06 -0300

    first commit

---
+++ Desktop/AvaliacaoLp3/Controllers/LojaController.cs
@@ -0,0 +1,67 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AvaliacaoLp3.Models;
+using AvaliacaoLp3.ViewModels;
+
+namespace AvaliacaoLp3.Controllers;
+
+public class LojaController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public static List<LojaViewModel> lojas = new List<LojaViewModel>();
+
+    public LojaController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult GerenciamentoLojas(){
+        return View(lojas);
+    }
+
+    public IActionResult DetalheLoja(int id){       
+
+        return View(lojas[id]);
+    }
+
+    public IActionResult CadastrarLoja(){
+                
+        return View();
+    }
+
+    public IActionResult Cadastramento([FromForm] string nome, [FromForm] string email, [FromForm] string categoria, [FromForm] string descricao, [FromForm] int piso){
+        
+        foreach (var loja in lojas)
+        {
+            if(nome == loja.Nome){
+                ViewData["Nome"] = nome;
+                return View();
+            }
+        }        
+
+        LojaViewModel estabelecimento = new LojaViewModel(lojas.Count(), piso, nome, descricao, categoria, email);
+        lojas.Add(estabelecimento);
+
+        return View("CadastrarLoja");
+    }
+
+    public IActionResult Deletar(int id){
+        if(lojas.Count() == 1){
+            lojas.Clear();
+        }else
+            lojas.RemoveAt(id);
+
+        foreach (var loja in lojas)
+        {
+            loja.Id = lojas.IndexOf(loja);
+        }
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
