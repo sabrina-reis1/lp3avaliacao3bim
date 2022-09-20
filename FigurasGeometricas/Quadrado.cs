016413a0545727e470b1b9a1c07216aa23a37000
sabrina-reis1 <101716378+sabrina-reis1@users.noreply.github.com>
2022-09-19 23:32:00 -0300

    apagando diretório errado

--- FigurasGeometricas/Quadrado.cs
+++
@@ -1,8 +0,0 @@
-public class Quadrado
-{
-    public double Lado { get; set; }
-    public double Area { get => Lado * Lado; }
-    public double Perimetro { get => 4 * Lado; }
-
-    public Quadrado(double lado) => Lado = lado;
-}
